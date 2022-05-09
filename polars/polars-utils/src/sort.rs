47bbed067a9f49c5762a072747f1a38569e8fe6c
Dmytro Samborskyy <dmytro.samborskyy@genesys.com>
2022-05-09 14:27:02 -0700

    Initial changes needed to compile and run for wasm in browser

--- polars/polars-utils/src/sort.rs
+++ polars/polars-utils/src/sort.rs
@@ -14,28 +14,40 @@ use rayon::{prelude::*, ThreadPool};
 ///
 /// # Safety
 /// The caller must ensure that the right indexes fo `&[(_, IdxSize)]` are integers ranging from `0..idx.len`
-pub unsafe fn perfect_sort(pool: &ThreadPool, idx: &[(IdxSize, IdxSize)]) -> Vec<IdxSize> {
-    let chunk_size = std::cmp::max(
-        idx.len() / pool.current_num_threads(),
-        pool.current_num_threads(),
-    );
 
-    let mut out: Vec<IdxSize> = Vec::with_capacity(idx.len());
-    let ptr = out.as_mut_ptr() as *const IdxSize as usize;
+macro_rules! perfect_sort_inner {
+    ($pool:expr, $idx:expr) => {{
+        let chunk_size = std::cmp::max(
+            $idx.len() / $pool.current_num_threads(),
+            $pool.current_num_threads(),
+        );
 
-    pool.install(|| {
-        idx.par_chunks(chunk_size).for_each(|indices| {
-            let ptr = ptr as *mut IdxSize;
-            for (idx_val, idx_location) in indices {
-                // Safety:
-                // idx_location is in bounds by invariant of this function
-                // and we ensured we have at least `idx.len()` capacity
-                *ptr.add(*idx_location as usize) = *idx_val;
-            }
+        let mut out: Vec<IdxSize> = Vec::with_capacity($idx.len());
+        let ptr = out.as_mut_ptr() as *const IdxSize as usize;
+
+        $pool.install(|| {
+            $idx.par_chunks(chunk_size).for_each(|indices| {
+                let ptr = ptr as *mut IdxSize;
+                for (idx_val, idx_location) in indices {
+                    // Safety:
+                    // idx_location is in bounds by invariant of this function
+                    // and we ensured we have at least `idx.len()` capacity
+                    *ptr.add(*idx_location as usize) = *idx_val;
+                }
+            });
         });
-    });
-    // Safety:
-    // all elements are written
-    out.set_len(idx.len());
-    out
+        // Safety:
+        // all elements are written
+        out.set_len($idx.len());
+        out
+    }};
+}
+
+#[cfg(not(target_family = "wasm"))]
+pub unsafe fn perfect_sort(pool: &ThreadPool, idx: &[(IdxSize, IdxSize)]) -> Vec<IdxSize> {
+    perfect_sort_inner!(pool, idx)
+}
+#[cfg(target_family = "wasm")]
+pub unsafe fn perfect_sort(pool: &crate::wasm::Pool, idx: &[(IdxSize, IdxSize)]) -> Vec<IdxSize> {
+    perfect_sort_inner!(pool, idx)
 }
